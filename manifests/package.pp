92d0b7e464c06a494aece2498ce00f4e715e57a5
Justin Lambert <jlambert@eml.cc>
2013-09-02 07:03:59 -0600

    remove installpath when ensure => absent

--- manifests/package.pp
+++ manifests/package.pp
@@ -134,7 +134,14 @@ class logstash::package {
       }
 
     } else {
-      ## Do we need to do anything when removing ?
+
+      # If not present, remove installpath, leave logfiles
+      file { $logstash::installpath:
+        ensure  => 'absent',
+        force   => true,
+        recurse => true,
+        purge   => true,
+      }
     }
 
   }
