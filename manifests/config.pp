28f8ea4ecdb7dc3739604ad5aec3b961ef56c942
Richard Pijnenburg <richard@ispavailability.com>
2014-01-30 12:18:06 +0100

    Fix config file location
    Reported on IRC that the location of the config file was wrong.

--- manifests/config.pp
+++ manifests/config.pp
@@ -47,15 +47,19 @@ class logstash::config {
       recurse => $logstash::purge_configdir
     }
 
+    file { "${logstash::configdir}/conf.d":
+      ensure => directory,
+    }
+
     file_concat { 'ls-config':
       ensure  => 'present',
       tag     => "LS_CONFIG_${::fqdn}",
-      path    => "${logstash::configdir}/logstash.conf",
+      path    => "${logstash::configdir}/conf.d/logstash.conf",
       owner   => $logstash::logstash_user,
       group   => $logstash::logstash_group,
       mode    => '0644',
       notify  => $notify_service,
-      require => File[$logstash::configdir]
+      require => File[ "${logstash::configdir}/conf.d" ]
     }
 
     file { $patterns_dir:
