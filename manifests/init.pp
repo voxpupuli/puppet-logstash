cff36bc39b7bc274bc220718d45273c22ca86e66
Richard Pijnenburg <richard@ispavailability.com>
2014-02-21 12:48:42 +0100

    Fail fast when using package_url and repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -142,6 +142,10 @@ class logstash(
     fail("\"${service_provider}\" is not a valid provider for \"${::operatingsystem}\"")
   }
 
+  if ($package_url != undef and $version != false) {
+    fail('Unable to set the version number when using package_url option.')
+  }
+
   validate_bool($manage_repo)
 
   if ($manage_repo == true) {
