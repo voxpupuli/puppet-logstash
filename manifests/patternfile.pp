ce015c56dcaeec0492c351a2c0d5320d28c9424e
Bryan Helmig <bryan@zapier.com>
2014-04-17 16:35:13 -0700

    match other config perms with patterns

--- manifests/patternfile.pp
+++ manifests/patternfile.pp
@@ -56,7 +56,7 @@ define logstash::patternfile (
     ensure  => 'file',
     owner   => $logstash::logstash_user,
     group   => $logstash::logstsah_group,
-    mode    => '0440',
+    mode    => '0644',
     source  => $source,
   }
 
