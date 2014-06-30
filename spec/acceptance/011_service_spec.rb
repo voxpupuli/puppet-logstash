0f26aab9d0e3705113b82b2aacf1e46e5c62c028
Richard Pijnenburg <richard@ispavailability.com>
2014-07-01 01:02:22 +0200

    [TESTING] Fix test

--- spec/acceptance/011_service_spec.rb
+++ spec/acceptance/011_service_spec.rb
@@ -23,7 +23,7 @@ describe "Service tests:" do
   describe "Make sure we can manage the defaults file" do
 
     context "Change the defaults file" do
-     it 'should run successfully' do
+      it 'should run successfully' do
         pp = "class { 'logstash': manage_repo => true, repo_version => '1.4', java_install => true, init_defaults => { 'LS_USER' => 'root', 'LS_JAVA_OPTS' => '\"-Djava.io.tmpdir=$HOME -XX:+UseTLAB\"' } }
               logstash::configfile { 'basic_config': content => 'input { tcp { port => 2000 } } output { null { } } ' }
              "
@@ -33,12 +33,10 @@ describe "Service tests:" do
         sleep 10
         expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
       end
-    end
 
-    if fact('osfamily') != 'Suse'
       describe service(service_name) do
         it { should be_enabled }
-        it { should be_running } 
+        it { should be_running }
       end
 
       describe package(package_name) do
@@ -49,9 +47,6 @@ describe "Service tests:" do
         it { should be_file }
         its(:content) { should match /[0-9]+/ }
       end
-    end
-
-    context "Make sure we have ES_USER=root" do
 
       describe file(defaults_file) do
         its(:content) { should match /^LS_USER=root/ }
@@ -59,11 +54,11 @@ describe "Service tests:" do
         its(:content) { should_not match /^LS_USER=logstash/ }
       end
 
-    end
 
-    it 'Show all running logstash processes' do
-      shell('ps auxfw | grep logstash | grep -v grep')
-    end
+      it 'Show all running logstash processes' do
+        shell('ps auxfw | grep logstash | grep -v grep')
+      end
 
+    end
   end
 end
