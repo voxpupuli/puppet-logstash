25aa33b36cd7aa65ce2bfa57d08d73747141fafc
Richard Pijnenburg <richard@ispavailability.com>
2014-08-03 20:37:53 +0000

    [TESTING] Add auto gpg import

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -35,7 +35,7 @@ hosts.each do |host|
     if fact('osfamily') == 'Suse'
       if fact('operatingsystem') == 'SLES'
         on host, "zypper --non-interactive ar --name puppet-devel http://download.opensuse.org/repositories/systemsmanagement:/puppet:/devel/SLE_11_SP3/ puppet-devel"
-        on host, "zypper --non-interactive refresh"
+        on host, "zypper --non-interactive --gpg-auto-import-keys refresh"
         install_package host 'rubygem-ruby-augeas libaugeas0 augeas-devel augeas-lenses'
       elsif fact('operatingsystem') == 'OpenSuSE'
         install_package host, 'ruby-devel augeas-devel libxml2-devel'
