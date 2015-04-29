b823b18dbd6a9094814bc92bd1f6f6d337add0ef
Brian Sparrow <bsparrow435@gmail.com>
2015-04-28 22:41:31 -0400

    Remove include_src as now defaults to false

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -42,7 +42,6 @@ class logstash::repo {
         release     => 'stable',
         repos       => 'main',
         key         => {'id' => '46095ACC8548582C1A2699A9D27D666CD88E42B4','server' => 'pgp.mit.edu'},
-        include_src => false,
       }
     }
     'RedHat': {
