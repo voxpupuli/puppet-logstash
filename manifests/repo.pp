a54ddf2447b482bdb42795ba960811d16bc1fc31
Brian Sparrow <bsparrow435@gmail.com>
2015-04-28 22:19:47 -0400

    key_server renamed to server in apt module 2.0.0+

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -42,7 +42,7 @@ class logstash::repo {
         release     => 'stable',
         repos       => 'main',
         key         => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
-        key_server  => 'pgp.mit.edu',
+        server      => 'pgp.mit.edu',
         include_src => false,
       }
     }
