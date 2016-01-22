2f74aeabb077d1d47839c1fa52d86b9c658370b8
Micha Krause <micha@krausam.de>
2016-01-22 13:03:39 +0100

    Added option to use content as input for patternfile

--- manifests/patternfile.pp
+++ manifests/patternfile.pp
@@ -11,7 +11,11 @@
 #   Puppet file resource of the pattern file ( puppet:// )
 #   Value type is string
 #   Default value: None
-#   This variable is required
+#
+# [*content*]
+#   Content of the Pattern file
+#   Value type is string
+#   Default value: None
 #
 # [*filename*]
 #   if you would like the actual file name to be different then the source file name
@@ -39,25 +43,34 @@
 # * Richard Pijnenburg <mailto:richard.pijnenburg@elasticsearch.com>
 #
 define logstash::patternfile (
-  $source,
+  $source = undef,
+  $content = undef,
   $filename = '',
 ){
 
-  validate_re($source, '^(puppet|file)://', 'Source must be either from a puppet fileserver or a locally accessible file (begins with either puppet:// or file://)' )
+  if $source {
+    validate_re($source, '^(puppet|file)://', 'Source must be from a puppet fileserver or a locally accessible file (begins with either puppet:// or file://)' )
+    $filename_real = $filename ? {
+      ''      => inline_template('<%= @source.split("/").last %>'),
+      default => $filename
+    }
+  }
+  elsif $content {
+    $filename_real = $title
+  }
+  else {
+    fail("you must specify ${source} or ${content}")
+  }
 
   $patterns_dir = "${logstash::configdir}/patterns"
 
-  $filename_real = $filename ? {
-    ''      => inline_template('<%= @source.split("/").last %>'),
-    default => $filename
-  }
 
   file { "${patterns_dir}/${filename_real}":
-    ensure => 'file',
-    owner  => $logstash::logstash_user,
-    group  => $logstash::logstash_group,
-    mode   => '0644',
-    source => $source,
+    ensure  => 'file',
+    owner   => $logstash::logstash_user,
+    group   => $logstash::logstash_group,
+    mode    => '0644',
+    source  => $source,
+    content => $content,
   }
-
 }
