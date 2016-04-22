a414de00b13a9515e5445504c9a5af457fbb9d59
Toby McLaughlin <toby@jarpy.net>
2016-04-22 10:56:31 +1000

    Assert correct Puppet version in tests.

--- manifests/init.pp
+++ manifests/init.pp
@@ -298,5 +298,4 @@ class logstash(
     -> Anchor['logstash::end']
 
   }
-
 }
--- spec/acceptance/001_basic_spec.rb
+++ spec/acceptance/001_basic_spec.rb
@@ -1,8 +1,24 @@
 require 'spec_helper_acceptance'
 
 # Here we put the more basic fundamental tests, ultra obvious stuff.
-describe "basic tests:" do
-  it 'make sure we have copied the module across' do
-    shell("ls #{default['distmoduledir']}/logstash/Modulefile", {:acceptable_exit_codes => 0})
+describe 'puppet' do
+  it 'should be the right version' do
+    expect(shell('puppet --version').stdout.chomp).to eq(PUPPET_VERSION)
+  end
+end
+
+describe 'logstash module' do
+  it 'should be available' do
+    shell(
+      "ls #{default['distmoduledir']}/logstash/Modulefile",
+      acceptable_exit_codes: 0
+    )
+  end
+
+  it 'should be parsable' do
+    shell(
+      "puppet parser validate #{default['distmoduledir']}/logstash/manifests/",
+      acceptable_exit_codes: 0
+    )
   end
 end
