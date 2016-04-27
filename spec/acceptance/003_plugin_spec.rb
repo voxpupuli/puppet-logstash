196b3a282a5f5506501755141e75e38b11470e60
Toby McLaughlin <toby@jarpy.net>
2016-04-27 16:59:02 +1000

    Refactor plugin tests.

--- spec/acceptance/003_plugin_spec.rb
+++ spec/acceptance/003_plugin_spec.rb
@@ -2,57 +2,58 @@
 require 'spec_helper_acceptance'
 
 describe 'class plugin' do
-  exe = '/opt/logstash/bin/plugin'
-  plugin = 'logstash-output-stdout'
+  let(:exe) { '/opt/logstash/bin/plugin' }
+  let(:plugin) { 'logstash-output-stdout' }
 
-  ensure_present = <<-END
-    logstash::plugin { '#{plugin}':
+  def ensure_present(extra_args = nil)
+    manifest = <<-END
+      logstash::plugin { '#{plugin}':
       ensure => present,
+      #{extra_args if extra_args}
     }
     END
+    apply_manifest(manifest, catch_failures: true)
+  end
 
-  ensure_absent = <<-END
+  def ensure_absent
+    manifest = <<-END
     logstash::plugin { '#{plugin}':
       ensure => absent,
     }
     END
+    apply_manifest(manifest, catch_failures: true)
+  end
+
+  def installed_plugins
+    shell("#{exe} list").stdout
+  end
 
   context 'with logstash installed' do
-    before(:all) do
-      install_logstash
-    end
+    before(:all) { install_logstash }
 
     context 'when a plugin is not installed' do
-      before(:each) do
-        shell("#{exe} uninstall #{plugin} || true")
-      end
+      before(:each) { shell("#{exe} uninstall #{plugin} || true") }
 
       it 'will not remove it again' do
-        log = apply_manifest(ensure_absent).stdout
-        expect(log).to_not contain("remove-#{plugin}")
+        expect(ensure_absent.stdout).to_not contain("remove-#{plugin}")
       end
 
       it 'can install the plugin from rubygems' do
-        log = apply_manifest(ensure_present).stdout
-        expect(log).to contain("install-#{plugin}")
-        expect(shell("#{exe} list").stdout).to contain(plugin)
+        ensure_present
+        expect(installed_plugins).to contain(plugin)
       end
     end
 
     context 'when a plugin is installed' do
-      before(:each) do
-        shell("#{exe} install #{plugin} || true")
-      end
+      before(:each) { shell("#{exe} install #{plugin} || true") }
 
       it 'will not install it again' do
-        log = apply_manifest(ensure_present).stdout
-        expect(log).to_not contain("install-#{plugin}")
+        expect(ensure_present.stdout).to_not contain("install-#{plugin}")
       end
 
       it 'can remove the plugin' do
-        log = apply_manifest(ensure_absent).stdout
-        expect(log).to contain("remove-#{plugin}")
-        expect(shell("#{exe} list").stdout).not_to contain(plugin)
+        ensure_absent
+        expect(installed_plugins).not_to contain(plugin)
       end
     end
   end
