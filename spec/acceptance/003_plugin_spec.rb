1ef67fc811f5219263b8928ed5aed1f7d97ff15c
Toby McLaughlin <toby@jarpy.net>
2016-04-28 16:40:21 +1000

    Stop Logstash before messing with plugins in tests.

--- spec/acceptance/003_plugin_spec.rb
+++ spec/acceptance/003_plugin_spec.rb
@@ -4,61 +4,63 @@ require 'spec_helper_acceptance'
 describe 'class plugin' do
   def ensure_plugin(present_absent, plugin, extra_args = nil)
     manifest = <<-END
+      class { 'logstash':
+        manage_repo  => true,
+        java_install => true,
+        status       => 'disabled',
+      }
+
       logstash::plugin { '#{plugin}':
         ensure => #{present_absent},
         #{extra_args if extra_args}
       }
       END
+
     apply_manifest(manifest, catch_failures: true)
   end
 
   def installed_plugins
-    shell("/opt/logstash/bin/plugin list").stdout
+    shell('/opt/logstash/bin/plugin list').stdout
   end
 
   def remove(plugin)
+    apply_manifest('service { "logstash": ensure => "stopped" }')
     shell("/opt/logstash/bin/plugin uninstall #{plugin} || true")
   end
 
-  context 'with logstash installed' do
-    before(:context) do
-      install_logstash
+  context 'when output-csv is not installed' do
+    before(:each) do
+      remove('logstash-output-csv')
     end
 
-    context 'when output-csv is not installed' do
-      before(:each) do
-        remove('logstash-output-csv')
-      end
-
-      it 'will not remove it again' do
-        log = ensure_plugin('absent', 'logstash-output-csv').stdout
-        expect(log).to_not contain('remove-logstash-output-csv')
-      end
-
-      it 'can install it from rubygems' do
-        ensure_plugin('present', 'logstash-output-csv')
-        expect(installed_plugins).to contain('logstash-output-csv')
-      end
+    it 'will not remove it again' do
+      log = ensure_plugin('absent', 'logstash-output-csv').stdout
+      expect(log).to_not contain('remove-logstash-output-csv')
     end
 
-    context 'when input-file is installed' do
-      before(:each) { expect(installed_plugins).to contain('logstash-input-file') }
+    it 'can install it from rubygems' do
+      ensure_plugin('present', 'logstash-output-csv')
+      expect(installed_plugins).to contain('logstash-output-csv')
+    end
+  end
 
-      it 'will not install it again' do
-        log = ensure_plugin('present', 'logstash-input-file').stdout
-        expect(log).to_not contain('install-logstash-input-file')
-      end
+  context 'when input-file is installed' do
+    before(:each) { expect(installed_plugins).to contain('logstash-input-file') }
 
-      it 'can remove it' do
-        ensure_plugin('absent', 'logstash-input-file')
-        expect(installed_plugins).not_to contain('logstash-input-file')
-      end
+    it 'will not install it again' do
+      log = ensure_plugin('present', 'logstash-input-file').stdout
+      expect(log).to_not contain('install-logstash-input-file')
     end
 
-    it 'can install a plugin from a local gem' do
-      plugin = 'logstash-output-cowsay'
-      ensure_plugin('present', plugin, "source => '/tmp/#{plugin}-0.1.0.gem'")
-      expect(installed_plugins).to contain(plugin)
+    it 'can remove it' do
+      ensure_plugin('absent', 'logstash-input-file')
+      expect(installed_plugins).not_to contain('logstash-input-file')
     end
+  end
+
+  it 'can install a plugin from a local gem' do
+    plugin = 'logstash-output-cowsay'
+    ensure_plugin('present', plugin, "source => '/tmp/#{plugin}-0.1.0.gem'")
+    expect(installed_plugins).to contain(plugin)
   end
 end
