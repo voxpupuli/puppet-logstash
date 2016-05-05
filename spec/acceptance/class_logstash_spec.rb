ad1a58279fa5e6011866688218ba0c31b84dd176
Toby McLaughlin <toby@jarpy.net>
2016-05-05 11:05:43 +1000

    Test "ensure => absent" is idempotent.

--- spec/acceptance/class_logstash_spec.rb
+++ spec/acceptance/class_logstash_spec.rb
@@ -82,6 +82,13 @@ describe 'class logstash' do
       remove_logstash
     end
 
+    it 'should be idempotent' do
+      if fact('lsbdistdescription') =~ /debian.*jessie/i
+        pending('https://github.com/elastic/puppet-logstash/issues/266')
+      end
+      apply_manifest(remove_logstash_manifest, catch_changes: true)
+    end
+
     describe package('logstash') do
       it { should_not be_installed }
     end
