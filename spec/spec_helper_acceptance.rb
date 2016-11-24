b06e990dde2b98259a8285676ca126859444fd94
Toby McLaughlin <toby@jarpy.net>
2016-11-23 21:32:23 -0500

    Remove java module from test containers

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -237,7 +237,6 @@ hosts.each do |host|
   install_dev_puppet_module_on(host, source: project_root, module_name: 'logstash')
 
   # Also install any other modules we need on the test system.
-  install_puppet_module_via_pmt_on(host, module_name: 'puppetlabs-java')
   install_puppet_module_via_pmt_on(host, module_name: 'puppetlabs-stdlib')
   install_puppet_module_via_pmt_on(host, module_name: 'puppetlabs-apt')
   install_puppet_module_via_pmt_on(host, module_name: 'electrical-file_concat')
