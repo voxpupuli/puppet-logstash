c24a2a848ba6718fdfe335b3531c1d56a7752e5b
Tim Meusel <tim@bastelfreak.de>
2017-03-26 00:47:34 +0100

    enhance spec testing

--- spec/classes/logstash_spec.rb
+++ spec/classes/logstash_spec.rb
@@ -11,6 +11,19 @@ describe 'logstash' do
       end
       context 'with all defaults' do
         it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('logstash') }
+        it { is_expected.to contain_class('logstash::config') }
+        it { is_expected.to contain_class('logstash::package') }
+        it { is_expected.to contain_class('logstash::params') }
+        it { is_expected.to contain_class('logstash::service') }
+        it { is_expected.to contain_file('/etc/logstash/conf.d') }
+        it { is_expected.to contain_file('/etc/logstash/jvm.options') }
+        it { is_expected.to contain_file('/etc/logstash/logstash.yml') }
+        it { is_expected.to contain_file('/etc/logstash/patterns') }
+        it { is_expected.to contain_file('/etc/logstash/startup.options') }
+        it { is_expected.to contain_file('/etc/logstash') }
+        it { is_expected.to contain_service('logstash') }
+        it { is_expected.to contain_package('logstash') }
       end
     end
   end
