d4b1f013188b196580627773ce145d3c6b68bebf
Toby McLaughlin <toby@jarpy.net>
2017-04-07 13:41:12 +1000

    Run plugin tests on Travis

--- spec/acceptance/class_plugin_spec.rb
+++ spec/acceptance/class_plugin_spec.rb
@@ -5,9 +5,10 @@ describe 'class plugin' do
   def ensure_plugin(present_absent, plugin, extra_args = nil)
     manifest = <<-END
       class { 'logstash':
-        status => 'disabled',
+        status            => 'disabled',
+        restart_on_change => false,
       }
-
+      ->
       logstash::plugin { '#{plugin}':
         ensure => #{present_absent},
         #{extra_args if extra_args}
@@ -24,60 +25,49 @@ describe 'class plugin' do
     shell("/usr/share/logstash/bin/logstash-plugin remove #{plugin} || true")
   end
 
-  before(:all) do
-    install_logstash_from_local_file
+  context 'when a plugin is not installed' do
+    before(:each) do
+      remove('logstash-input-sqs')
+    end
+
+    it 'will not remove it again' do
+      log = ensure_plugin('absent', 'logstash-input-sqs').stdout
+      expect(log).to_not contain('remove-logstash-input-sqs')
+    end
+
+    it 'can install it from rubygems' do
+      ensure_plugin('present', 'logstash-input-sqs')
+      expect(installed_plugins).to contain('logstash-input-sqs')
+    end
   end
 
-  before(:each) do
-    stop_logstash
+  context 'when a plugin is installed' do
+    before(:each) do
+      expect(installed_plugins).to contain('logstash-input-file')
+    end
+
+    it 'will not install it again' do
+      log = ensure_plugin('present', 'logstash-input-file').stdout
+      expect(log).to_not contain('install-logstash-input-file')
+    end
+
+    it 'can remove it' do
+      ensure_plugin('absent', 'logstash-input-file')
+      expect(installed_plugins).not_to contain('logstash-input-file')
+    end
   end
 
-  unless ENV['TRAVIS_CI']
-    # These tests are inexplicably, maddeningly slow under Travis CI.
-    context 'when a plugin is not installed' do
-      before(:each) do
-        remove('logstash-input-sqlite')
-      end
+  it 'can install a plugin from a "puppet://" url' do
+    plugin = 'logstash-output-cowthink'
+    source = "puppet:///modules/logstash/#{plugin}-5.0.0.gem"
+    ensure_plugin('present', plugin, "source => '#{source}'")
+    expect(installed_plugins).to contain(plugin)
+  end
 
-      it 'will not remove it again' do
-        log = ensure_plugin('absent', 'logstash-input-sqlite').stdout
-        expect(log).to_not contain('remove-logstash-input-sqlite')
-      end
-
-      it 'can install it from rubygems' do
-        ensure_plugin('present', 'logstash-input-sqlite')
-        expect(installed_plugins).to contain('logstash-input-sqlite')
-      end
-    end
-
-    context 'when a plugin is installed' do
-      before(:each) do
-        expect(installed_plugins).to contain('logstash-input-file')
-      end
-
-      it 'will not install it again' do
-        log = ensure_plugin('present', 'logstash-input-file').stdout
-        expect(log).to_not contain('install-logstash-input-file')
-      end
-
-      it 'can remove it' do
-        ensure_plugin('absent', 'logstash-input-file')
-        expect(installed_plugins).not_to contain('logstash-input-file')
-      end
-    end
-
-    it 'can install a plugin from a "puppet://" url' do
-      plugin = 'logstash-output-cowthink'
-      source = "puppet:///modules/logstash/#{plugin}-5.0.0.gem"
-      ensure_plugin('present', plugin, "source => '#{source}'")
-      expect(installed_plugins).to contain(plugin)
-    end
-
-    it 'can install a plugin from a local gem' do
-      plugin = 'logstash-output-cowsay'
-      source = "/tmp/#{plugin}-5.0.0.gem"
-      ensure_plugin('present', plugin, "source => '#{source}'")
-      expect(installed_plugins).to contain(plugin)
-    end
+  it 'can install a plugin from a local gem' do
+    plugin = 'logstash-output-cowsay'
+    source = "/tmp/#{plugin}-5.0.0.gem"
+    ensure_plugin('present', plugin, "source => '#{source}'")
+    expect(installed_plugins).to contain(plugin)
   end
 end
